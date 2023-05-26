420a0647df4ad1e7e2e92760940c5081568ca4fe
Nyles Pham <85137552+nylespham@users.noreply.github.com>
2023-05-26 17:14:19 +0700

    Update HelloController.java (#1)

--- src/main/java/com/course/tomcat/controller/HelloController.java
+++ src/main/java/com/course/tomcat/controller/HelloController.java
@@ -18,4 +18,9 @@ public class HelloController {
     int random(){
         return ThreadLocalRandom.current().nextInt(0,1000);
     }
+    
+    @GetMapping(value = "/hero")
+    String hero(){
+        return "My favorite superhero is Batman";
+    }
 }
