8dec63f87ce1108a8e24ea29f17af14a44415058
Nyles Pham <85137552+nylespham@users.noreply.github.com>
2023-05-26 16:39:37 +0700

    Update HelloController.java

--- src/main/java/com/course/tomcat/controller/HelloController.java
+++ src/main/java/com/course/tomcat/controller/HelloController.java
@@ -3,6 +3,7 @@ package com.course.tomcat.controller;
 import org.springframework.web.bind.annotation.*;
 
 import java.time.LocalDateTime;
+import java.util.concurrent.ThreadLocalRandom;
 
 @RestController
 @RequestMapping("/api")
@@ -12,5 +13,9 @@ public class HelloController {
     String hello() {
         return "Now is " + LocalDateTime.now();
     }
-
+    
+    @GetMapping(value = "/random")
+    int random(){
+        return ThreadLocalRandom.current().nextInt(0,1000);
+    }
 }
