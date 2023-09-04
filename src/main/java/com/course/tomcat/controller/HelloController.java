63f9ab7de92c80f5956aede24400d3962cef2f0d
natalequaratodxc <52038623+natalequaratodxc@users.noreply.github.com>
2023-09-04 12:35:10 +0000

    modified:   src/main/java/com/course/tomcat/controller/HelloController.java

--- src/main/java/com/course/tomcat/controller/HelloController.java
+++ src/main/java/com/course/tomcat/controller/HelloController.java
@@ -13,4 +13,9 @@ public class HelloController {
         return "Now is " + LocalDateTime.now();
     }
 
+    @GetMapping(value = "/random")
+    int random(){
+        return ThreadLocalRandom.current().nextInt(0,1000);
+    }
+
 }
