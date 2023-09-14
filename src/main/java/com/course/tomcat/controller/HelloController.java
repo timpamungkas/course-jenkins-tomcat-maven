f2eb25e1785873b50e2a299aec58a102144e4b07
priyankach-19 <137378156+priyankach-19@users.noreply.github.com>
2023-09-13 19:34:05 -0700

    Update HelloController.java

--- src/main/java/com/course/tomcat/controller/HelloController.java
+++ src/main/java/com/course/tomcat/controller/HelloController.java
@@ -14,7 +14,10 @@ public class HelloController {
         return "Now is " + LocalDateTime.now();
     }
     
-    
+    @GetMapping(value = "/random")
+    int random(){
+        return ThreadLocalRandom.current().nextInt(0,1000);
+    }
  
     @GetMapping(value = "/hero")
     String hero(){
