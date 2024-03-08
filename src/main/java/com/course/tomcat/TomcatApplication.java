ebb272372adf8287aa8daef77338cab607256d76
draganoff07 <122620774+draganoff07@users.noreply.github.com>
2024-03-08 12:36:51 +0200

    Update TomcatApplication.java

--- src/main/java/com/course/tomcat/TomcatApplication.java
+++ src/main/java/com/course/tomcat/TomcatApplication.java
@@ -1,14 +1,21 @@
-package com.course.tomcat;
-
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
-
-@SpringBootApplication
-public class TomcatApplication extends SpringBootServletInitializer {
-
-	public static void main(String[] args) {
-		SpringApplication.run(TomcatApplication.class, args);
-	}
-
+package com.course.tomcat.controller;
+ 
+import org.springframework.web.bind.annotation.*;
+ 
+import java.time.LocalDateTime;
+import java.util.concurrent.ThreadLocalRandom;
+ 
+@RestController
+@RequestMapping("/api")
+public class HelloController {
+ 
+    @GetMapping(value = {"", "/", "/now"})
+    String hello() {
+        return "Now is " + LocalDateTime.now();
+    }
+    
+    @GetMapping(value = "/random")
+    int random(){
+        return ThreadLocalRandom.current().nextInt(0,1000);
+    }
 }
