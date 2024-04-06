8a209c7bdf1e970761ed77e5ea21f997fb83ab58
Jefferson Souza <je_jss@hotmail.com>
2024-04-06 02:22:29 -0300

    created random method on controller

--- src/main/java/com/course/tomcat/controller/HelloController.java
+++ src/main/java/com/course/tomcat/controller/HelloController.java
@@ -1,6 +1,8 @@
 package com.course.tomcat.controller;
 
 import org.springframework.web.bind.annotation.*;
+import java.util.concurrent.ThreadLocalRandom;
+
 
 import java.time.LocalDateTime;
 
@@ -11,6 +13,11 @@ public class HelloController {
     @GetMapping(value = {"", "/", "/now"})
     String hello() {
         return "Now is " + LocalDateTime.now();
+    }
+
+    @GetMapping("random")
+    int random() {
+        return ThreadLocalRandom.current().nextInt(0,1000);
     }
 
 }
